4f559b4344c885488d66923d7a5e3918f8d39423
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-09 18:13:29 +0100

    working with js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-//alert("probando");
+//=========== Variables ========
+const plusIcon = [...document.querySelectorAll(".plus-icon")];
+const minusIcon = [...document.querySelectorAll(".minus-icon")];
+console.log(minusIcon);
+const questionsText= [...document.querySelectorAll(".question-text")];
+//console.log(questionsText);
+
+//======== Add Event Listener ==========
+let numeroArray = 0;
+
+
+
+
+plusIcon.forEach(icon =>{
+    icon.addEventListener("click",()=>{
+        sacarTexto(numeroArray);
+        numeroArray++;
+    })
+})
+
+minusIcon.forEach(iconMinus =>{
+    iconMinus.addEventListener("click", ()=>{
+        cambiarIcono(numeroArray);
+    })
+})
+
+
+const sacarTexto = function(elemento){
+    questionsText[numeroArray].style.display = "block";
+
+}
+
+const cambiarIcono = function(elemento){
+    elemento.style.display = "block";
+}
