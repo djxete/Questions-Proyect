25812afcffa9195b88612f09014ec1e86d8b91d0
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-10 13:41:13 +0100

    another way to do it

--- js/main.js
+++ js/main.js
@@ -1,21 +1,49 @@
-//=========== Variables ========
+// /*=========== Variables ========*/
 
-const btns = [...document.querySelectorAll(".question-btn")];
+// const btns = [...document.querySelectorAll(".question-btn")];
+
+// /*=========== Add Event Listener and functions ========*/
+
+
+// btns.forEach(btn => {
+//     btn.addEventListener("click", (e) => {
+//         //console.log(e.currentTarget);
+//         const siblingElement = e.currentTarget.parentElement.parentElement;
+
+//         siblingElement.classList.toggle("show-text");
+
+//     })
+// })
+
+
+//=================== Another way to do it ========
+
+//------ Variables ----
+
+const articles = document.querySelectorAll(".question");
 
 //=========== Add Event Listener and functions ========
 
+articles.forEach(article => {
 
-btns.forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        //console.log(e.currentTarget);
-        const siblingElement = e.currentTarget.parentElement.parentElement;
 
-        siblingElement.classList.toggle("show-text");
+    let btnArticle = article.querySelector(".question-btn");
+
+    btnArticle.addEventListener("click", () => {
+
+        articles.forEach(question => {
+            
+            if(question !== article){
+                question.classList.remove("show-text")
+            }
+
+        article.classList.toggle("show-text");
+
 
     })
+
+
 })
 
-
-
-
+})
 
